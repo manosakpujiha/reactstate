8da8da0e77a25d446ec5f3d59b7437f66f4a0dbb
manosakpujiha <manos.akpujiha@gmail.com>
2023-02-08 14:01:14 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import Clicker from './Clicker';
-import Dice from './Dice'
-import Button from './Button'
 import RollDice from './RollDice';
 
 function App() {
   return (
     <div className="App">
      {/* <Clicker/> */}
-     {/* <Dice dice='five'/> */}
-     {/* <Dice dice='five'/> */}
       <RollDice />
-    {/* <Button/> */}
     </div>
   );
 }
--- src/Dice.js
+++ src/Dice.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Dice.css';
-
 export default class Dice extends Component {
     constructor (props) {
         super(props)
